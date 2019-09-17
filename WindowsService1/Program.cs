f5d2db16b39edd4f591fa3a53e700bfe6143eb47
umlstudy <umlstudy@gmail.com>
2019-09-17 15:29:10 +0900

    init...

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace WindowsService1
+{
+
+    static class Program
+    {
+
+        private static ILog log = LogManager.GetLogger("Program");
+
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        static void Main()
+        {
+            log.Info("MyService starting..");
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service1() 
+			};
+
+            ServiceBase.Run(ServicesToRun);
+            log.Info("MyService started..");
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using System.ServiceProcess;
+using System.Net.Sockets;
+using System;
+using log4net;
+using System.Threading;
+using System.Text;
+using System.Diagnostics;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private static ILog log = LogManager.GetLogger("Service1");
+
+        private Boolean running;
+
+        public Service1()
+        {
+            InitializeComponent();
+            running = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(Run));
+            t1.Start();
+        }
+
+        void Run()
+        {
+            try
+            {
+                RunReal();
+            }
+            catch (Exception ex)
+            {
+                log.Debug("error", ex);
+            }
+        }
+
+        void RunReal()
+        {
+            log.Debug("Create udp Socket...");
+            
+            IPEndPoint ipEndPoing = new IPEndPoint(IPAddress.Any, 9050);
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(ipEndPoing);
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+
+            byte[] data = new byte[1024];
+            int recv;
+
+            while (running)
+            {
+                data = new byte[1024];
+                recv = udpSocket.ReceiveFrom(data, ref Remote);
+
+                log.Debug(String.Format("Message received from {0}:", Remote.ToString()));
+
+                string recvString = Encoding.ASCII.GetString(data, 0, recv);
+                log.Debug(recvString);
+
+                // udpSocket.SendTo(data, recv, SocketFlags.None, Remote);
+
+                if (recvString.StartsWith("rebootnow!!") )
+                {
+                    log.Debug("shutdown cmd run...");
+                    runCommand("shutdown -f -r -t 0");
+
+                    //System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    //startInfo.FileName = "cmd.exe";
+                    //startInfo.Arguments = "/C shutdown -f -r -t 0";
+                    //process.StartInfo = startInfo;
+                    //process.Start();
+                }
+            }
+        }
+
+        static void runCommand(string cmd)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c " + cmd;
+            process.StartInfo.UseShellExecute = false;
+            // process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            //* Set your output and error (asynchronous) handlers
+            process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+            //* Start process and handlers
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+        }
+
+        static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            log.Debug(outLine.Data);
+        }
+
+        protected override void OnStop()
+        {
+            running = false;
+        }
+    }
+}
